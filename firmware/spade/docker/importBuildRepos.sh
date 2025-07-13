22f537c76eddc515208b6bba1e0c4a0d49af6218
Marios Mitsios <xrteach@gmail.com>
2025-07-13 14:46:59 -0400

    Update importBuildRepos.sh

--- firmware/spade/docker/importBuildRepos.sh
+++ firmware/spade/docker/importBuildRepos.sh
@@ -5,11 +5,11 @@ cd jerryscript
 
 mkdir ~/raspberrypi
 cd ~/raspberrypi
-git clone -b 1.3.1 https://github.com/raspberrypi/pico-sdk.git
+git clone https://github.com/GalaxyGamingBoy/rpi-pico-sdk.git pico-sdk
 git clone https://github.com/raspberrypi/pico-extras.git
 cd pico-sdk
-git checkout 7070d230c0cdf1add83fa86b6832b47b2da47788
+#git checkout 7070d230c0cdf1add83fa86b6832b47b2da47788
 git submodule update --init
 cd ../pico-extras
-git checkout 09c64d509f1d7a49ceabde699ed6c74c77e195a1
+#git checkout 09c64d509f1d7a49ceabde699ed6c74c77e195a1
 git submodule update --init
